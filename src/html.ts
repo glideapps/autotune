5c17f4573802df6f881d3094240f0ebf05e31b9f
David Siegel <djsiegel@gmail.com>
2018-05-30 18:26:38 -0700

    Organize

--- src/html.ts
+++ src/html.ts
@@ -1,64 +1,42 @@
 import { OptionValue, oneOf } from ".";
 
-export function startHTMLExperiments() {
-  // First gather attribute-based experiments like:
-  //
-  //   <h1 data-experiment="hero" data-option="a">Welcome!</h1>
-  //   <h1 data-experiment="hero" data-option="b">¡Bienvenidos!</h1>
-  //
+type AttributeExperiments = {
+  [name: string]: {
+    options: OptionValue[];
+    nodes: Element[];
+  };
+};
 
-  let attributeBasedExperiments: {
-    [name: string]: {
-      options: OptionValue[];
-      nodes: Element[];
-    };
-  } = {};
+type TagExperiments = {
+  [name: string]: Array<{
+    options: OptionValue[];
+    node: Element;
+  }>;
+};
 
-  const attributedNodes = document.querySelectorAll("[data-experiment]");
-  for (let i = 0; i < attributedNodes.length; i++) {
-    const node = attributedNodes.item(i);
-    const name = node.getAttribute("data-experiment");
-    const option = node.getAttribute("data-option");
-
-    let data = attributeBasedExperiments[name];
-    if (data === undefined) {
-      data = attributeBasedExperiments[name] = { options: [], nodes: [] };
-    }
-    if (data.options.indexOf(option) === -1) {
-      data.options.push(option);
-    }
-    data.nodes.push(node);
+function hash(s: string): number {
+  var hash = 0;
+  for (var i = 0; i < s.length; i++) {
+    var character = s.charCodeAt(i);
+    hash = (hash << 5) - hash + character;
+    hash = hash & hash; // Convert to 32bit integer
   }
+  return hash;
+}
 
-  // Then gather tag-based experiments like:
-  //
-  //   <experiment name="hero">
-  //     <h1>Hello!</h1>
-  //     <h1>World!</h1>
-  //   </experiment>
-  //
+function optionNodeLabelOrText(node: Element): string {
+  return node.getAttribute("label") || hash(node.outerHTML).toString();
+}
 
-  let tagBasedExperiments: {
-    [name: string]: Array<{
-      options: OptionValue[];
-      node: Element;
-    }>;
-  } = {};
-
-  function optionNodeLabelOrText(node: Element): string {
-    function hash(s: string): number {
-      var hash = 0;
-      for (var i = 0; i < s.length; i++) {
-        var character = s.charCodeAt(i);
-        hash = (hash << 5) - hash + character;
-        hash = hash & hash; // Convert to 32bit integer
-      }
-      return hash;
-    }
-
-    const optionLabel = node.getAttribute("label");
-    return optionLabel || hash(node.outerHTML).toString();
-  }
+// Gather tag-based experiments like:
+//
+//   <experiment name="hero">
+//     <h1>Hello!</h1>
+//     <h1>World!</h1>
+//   </experiment>
+//
+function getTagExperiments(): TagExperiments {
+  let experiments: TagExperiments = {};
 
   const customTagNodes = document.getElementsByTagName("experiment");
   for (let i = 0; i < customTagNodes.length; i++) {
@@ -71,13 +49,44 @@ export function startHTMLExperiments() {
       data.options.push(optionNodeLabelOrText(child));
     }
 
-    const list = tagBasedExperiments[name] || [];
+    const list = experiments[name] || [];
     list.push(data);
-    tagBasedExperiments[name] = list;
+    experiments[name] = list;
   }
+  return experiments;
+}
 
-  Object.getOwnPropertyNames(attributeBasedExperiments).forEach(name => {
-    const { options, nodes } = attributeBasedExperiments[name];
+// Gather attribute-based experiments like:
+//
+//   <h1 data-experiment="hero" data-option="a">Welcome!</h1>
+//   <h1 data-experiment="hero" data-option="b">¡Bienvenidos!</h1>
+//
+function getAttributeExperiments(): AttributeExperiments {
+  let experiments: AttributeExperiments = {};
+  const attributedNodes = document.querySelectorAll("[data-experiment]");
+  for (let i = 0; i < attributedNodes.length; i++) {
+    const node = attributedNodes.item(i);
+    const name = node.getAttribute("data-experiment");
+    const option = node.getAttribute("data-option");
+
+    let data = experiments[name];
+    if (data === undefined) {
+      data = experiments[name] = { options: [], nodes: [] };
+    }
+    if (data.options.indexOf(option) === -1) {
+      data.options.push(option);
+    }
+    data.nodes.push(node);
+  }
+  return experiments;
+}
+
+export function startHTMLExperiments() {
+  const attributeExperiments = getAttributeExperiments();
+  const tagExperiments = getTagExperiments();
+
+  Object.getOwnPropertyNames(attributeExperiments).forEach(name => {
+    const { options, nodes } = attributeExperiments[name];
     const choice = oneOf(name, options);
     nodes.forEach(node => {
       if (node.getAttribute("data-option") !== choice) {
@@ -86,14 +95,14 @@ export function startHTMLExperiments() {
     });
   });
 
-  Object.getOwnPropertyNames(tagBasedExperiments).forEach(name => {
+  Object.getOwnPropertyNames(tagExperiments).forEach(name => {
     let options: { [value: string]: null } = {};
     // Gather unique option values
-    tagBasedExperiments[name].forEach(experiment => {
+    tagExperiments[name].forEach(experiment => {
       experiment.options.forEach(o => (options[o] = null));
     });
     const choice = oneOf(name, Object.getOwnPropertyNames(options));
-    tagBasedExperiments[name].forEach(experiment => {
+    tagExperiments[name].forEach(experiment => {
       const children = experiment.node.children;
       for (let i = 0; i < children.length; i++) {
         const child = children.item(i);
