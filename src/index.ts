762d9323e9880393eba20aa3c239e923f32056e1
David Siegel <dasiege@microsoft.com>
2018-06-02 09:08:27 -0700

    Move helper functions to util.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import { startHTMLExperiments } from "./html";
-
-// https://stackoverflow.com/a/2117523/80410
-function uuidv4() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
-
-const DEBUG = true;
+import { http, uuidv4, log, mapObject, getOwnPropertyValues } from "./util";
 
 export type OptionValue = string;
 
@@ -61,25 +51,6 @@ const state: {
   queuedCompletedExperiments: {},
   queuedStartedExperiments: {}
 };
-
-function log(...args: any[]) {
-  if (DEBUG) {
-    console.log("AUTOTUNE", ...args);
-  }
-}
-
-function getOwnPropertyValues<T>(x: { [name: string]: T }): T[] {
-  return Object.getOwnPropertyNames(x).map(n => x[n]);
-}
-
-function mapObject<T, S>(
-  x: { [name: string]: T },
-  f: (v: T, k: string) => S
-): { [name: string]: S } {
-  let result: { [name: string]: S } = {};
-  Object.getOwnPropertyNames(x).forEach(n => (result[n] = f(x[n], n)));
-  return result;
-}
 
 function startExperiment(experiment: Experiment<OptionValue>): void {
   if (state.startExperimentsTimer !== undefined) {
@@ -154,37 +125,6 @@ function completeExperiment(
       }
     }
   }, 10);
-}
-
-function http(
-  method: "POST" | "GET",
-  url: string,
-  data: any = undefined
-): Promise<any> {
-  return new Promise((resolve, reject) => {
-    if (typeof XMLHttpRequest === "undefined") {
-      // TODO support node
-      return reject("Not running in browser");
-    }
-
-    let request = new XMLHttpRequest();
-    request.open(method, url, true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.onerror = () => reject(request.statusText);
-    request.onreadystatechange = () => {
-      if (request.readyState == 4)
-        if (request.status == 200) {
-          resolve(JSON.parse(request.responseText));
-        } else {
-          reject(`Request failed with status ${request.status}`);
-        }
-    };
-    if (data !== undefined) {
-      request.send(JSON.stringify(data));
-    } else {
-      request.send();
-    }
-  });
 }
 
 export async function initialize(appKey: string): Promise<{}> {
---
+++ src/util.ts
@@ -0,0 +1,60 @@
+const DEBUG = true;
+
+// https://stackoverflow.com/a/2117523/80410
+export function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+    let r = (Math.random() * 16) | 0;
+    let v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+export function log(...args: any[]) {
+  if (DEBUG) {
+    console.log("AUTOTUNE", ...args);
+  }
+}
+
+export function getOwnPropertyValues<T>(x: { [name: string]: T }): T[] {
+  return Object.getOwnPropertyNames(x).map(n => x[n]);
+}
+
+export function mapObject<T, S>(
+  x: { [name: string]: T },
+  f: (v: T, k: string) => S
+): { [name: string]: S } {
+  let result: { [name: string]: S } = {};
+  Object.getOwnPropertyNames(x).forEach(n => (result[n] = f(x[n], n)));
+  return result;
+}
+
+export function http(
+  method: "POST" | "GET",
+  url: string,
+  data: any = undefined
+): Promise<any> {
+  return new Promise((resolve, reject) => {
+    if (typeof XMLHttpRequest === "undefined") {
+      // TODO support node
+      return reject("Not running in browser");
+    }
+
+    let request = new XMLHttpRequest();
+    request.open(method, url, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onerror = () => reject(request.statusText);
+    request.onreadystatechange = () => {
+      if (request.readyState == 4)
+        if (request.status == 200) {
+          resolve(JSON.parse(request.responseText));
+        } else {
+          reject(`Request failed with status ${request.status}`);
+        }
+    };
+    if (data !== undefined) {
+      request.send(JSON.stringify(data));
+    } else {
+      request.send();
+    }
+  });
+}
