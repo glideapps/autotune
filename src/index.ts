a0155bcb32bed2651fc745fae37c0bb1cd7a1815
Mark Probst <mark.probst@gmail.com>
2018-06-09 08:24:03 -0700

    Send whether random option is used. Fixes #13

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,8 @@ function startExperiment(theExperiment: Experiment): void {
         let experiments = mapObject(state.queuedStartedExperiments, e => ({
             instanceKey: e.key,
             options: e.options,
-            pick: e.pick
+            pick: e.pick,
+            pickedBest: e.pickedBest
         }));
 
         log("Starting experiments", experiments);
@@ -171,6 +172,7 @@ function experiment(name: string): Experiment {
 export class Experiment {
     payoff: number;
     pick?: string;
+    pickedBest?: boolean;
     options: string[];
 
     readonly key: string;
@@ -183,7 +185,8 @@ export class Experiment {
         this.key = uuidv4();
     }
 
-    private setValueAndStartExperiment(value: string): string {
+    private setValueAndStartExperiment(value: string, pickedBest: boolean): string {
+        this.pickedBest = pickedBest;
         if (this.pick === undefined) {
             this.pick = value;
             startExperiment(this);
@@ -204,14 +207,15 @@ export class Experiment {
     oneOf(...options: string[]): string {
         this.options = options;
 
+        const pickRandom = this.bestOption === undefined || Math.random() < this.epsilon;
         let one: string;
-        if (this.bestOption === undefined || Math.random() < this.epsilon) {
+        if (pickRandom) {
             one = options[Math.floor(Math.random() * options.length)];
         } else {
             one = this.bestOption;
         }
 
-        return this.setValueAndStartExperiment(one);
+        return this.setValueAndStartExperiment(one, !pickRandom);
     }
 }
 
