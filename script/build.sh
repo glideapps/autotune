14f2e546ee42f599833bef93e78e66680493bf7f
David Siegel <dasiege@microsoft.com>
2018-06-09 16:24:17 -0700

    Stop using GitHub pages

--- script/build.sh
+++ script/build.sh
@@ -9,5 +9,3 @@ pushd dist
 browserify index.js --standalone autotune -o index.browser.js
 uglifyjs index.browser.js --compress --mangle -o index.browser.min.js
 popd
-
-cp dist/index.browser.js docs/autotune.js
