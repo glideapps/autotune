5d7bec7c402b2c30f9a6336e9ccb14673f8bd434
David Siegel <dasiege@microsoft.com>
2018-06-17 16:39:04 -0700

    Trigger quicktype.io deploy after publish

--- script/publish.sh
+++ script/publish.sh
@@ -3,3 +3,11 @@
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 npm run bump-version
 npm publish
+
+# Publish quicktype.io
+curl -H "Authorization: Bearer $BUILDKITE_TOKEN" \
+    https://api.buildkite.com/v2/organizations/typeguard/pipelines/quicktype-dot-io/builds \
+    -X POST \
+    -F "commit=HEAD" \
+    -F "branch=master" \
+    -F "message=Deploy for new autotune :rocket:"
